77f2fb79515729ddd4133b6e21b27db42616dca4
Laura <laurasanchez0301@gmail.com>
2023-11-22 10:38:09 +0100

    hero page

--- src/heroes/pages/HeroPage.jsx
+++ src/heroes/pages/HeroPage.jsx
@@ -1,3 +1,51 @@
+import { Navigate, useNavigate, useParams } from "react-router-dom";
+import { getHeroById } from "../helpers";
+
 export const HeroPage = () => {
-  return <h1>Hero</h1>;
+  const { id } = useParams();
+  const hero = getHeroById(id);
+  const navigate = useNavigate();
+
+  const onNavigateBack = () => {
+    const publisherSplit = hero.publisher.split(" ")[0].toLowerCase();
+    navigate(`/${publisherSplit}`);
+  };
+
+  if (!hero) {
+    return <Navigate to="/marvel" />;
+  }
+
+  return (
+    <div className="row mt-5">
+      <div className="col-4">
+        <img
+          src={`/assets/heroes/${id}.jpg`}
+          alt={hero.superhero}
+          className="img-thumbnail"
+        />
+      </div>
+      <div className="col-8">
+        <h3>{hero.superhero}</h3>
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item">
+            <b>Alter ego: </b>
+            {hero.alter_ego}
+          </li>
+          <li className="list-group-item">
+            <b>Publisher: </b>
+            {hero.publisher}
+          </li>
+          <li className="list-group-item">
+            <b>First appearance: </b>
+            {hero.first_appearance}
+          </li>
+        </ul>
+        <h5 className="mt-3">Characters</h5>
+        <p>{hero.characters}</p>
+        <button onClick={onNavigateBack} className="btn btn-outline-primary">
+          BACK
+        </button>
+      </div>
+    </div>
+  );
 };
